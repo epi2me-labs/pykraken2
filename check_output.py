f03e6372a3536daf83e49119e3f3955f248312c5
Neil Horner <neil.horner@nanoporetech.com>
2022-05-05 14:55:24 +0100

    Commit before changeing return results control flow

--- check_output.py
+++ check_output.py
@@ -8,13 +8,19 @@ from collections import Counter
 
 # should be 500/502 results lines, getting 726
 ids = []
-with open('/Users/Neil.Horner/nf_dev/kraken/debug.tsv') as fh:
-    for line in fh:
-        ids.append(line.split('\t')[1])
+with open('/Volumes/Groups/custflow/active/nhorner/kraken/code/SRR17237156_1.tsv') as fh1, open('/Volumes/Groups/custflow/active/nhorner/kraken/code/SRR17237156_2.tsv') as fh2:
+    for i, (l1, l2) in enumerate(zip(fh1, fh2)):
+        # ids.append(line.split('\t')[1])
+        id1 = l1.split('\t')[1]
+        id2 = l2.split('\t')[1]
+        if id1 != id1:
+            print(i, id1, id2)
+            break
+    ...
 
-print(len(ids))
-c = Counter(ids)
-print(max(list(c.values())))
+# print(len(ids))
+# c = Counter(ids)
+# print(max(list(c.values())))
 
 
 
